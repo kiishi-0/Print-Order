debfc76a7ded539ec7faa798b872c625991229f2
kiishi-0 <kiishioluwa@gmail.com>
2022-02-12 21:52:19 +0100

    done with summary page

--- src/Pages/SummaryPage/SummaryPage.jsx
+++ src/Pages/SummaryPage/SummaryPage.jsx
@@ -16,39 +16,120 @@ class SummaryPage extends React.Component{
                     <div className='orders'>
                         <div className='order'>
                             <h3 className='order-title'>Order 1</h3>
-                            <div className='job-title-section'>
-                                <h4>JOB TITLE</h4>
+                            <div className='job-title-section sec'>
+                                <h4>JOB TITLE:</h4>
                                 <div className='j-title'>Lorem Ipsum</div>
                             </div>
-                            <div className='copies-date-sectiom'>
+                            <div className='copies-date-section sec'>
                                 <div className='page-section'>
-                                    <h4>PAGES</h4>
+                                    <h4>PAGES:</h4>
                                     <h6>20</h6>
                                 </div>
                                 <div className='copies-section'>
-                                    <h4>PAGES</h4>
-                                    <h6>20</h6>
+                                    <h4>COPIES:</h4>
+                                    <h6>450</h6>
                                 </div>
                                 <div className='date-section'>
-                                    <h4>PAGES</h4>
-                                    <h6>20</h6>
+                                    <h4>DATE DUE:</h4>
+                                    <h6>23/09/2022</h6>
                                 </div>
                             </div>
-                            <div className='address-section'>
+                            <div className='address-section sec'>
                                 <h4>ADDRESS:</h4>
                                 <div className='address'>
                                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                                         Ut tempor senectus arcu cursus tortor odio ornare </p>
                                 </div>
                             </div>
-                            <div className='info-section'>
+                            <div className='info-section sec'>
+                                <h4>ADDITIONAL INFORMATION:</h4>
                                 <div className='info'>
                                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                                     Ut tempor senectus arcu cursus tortor odio ornare velit 
                                     blandit. Elit faucibus ante sed nascetur. Tincidunt </p>
                                 </div>
                             </div>
-                            <div className='buttons-section'>
+                            <div className='buttons-section sec'>
+                                <button className='edit-btn'>EDIT</button>
+                                <button className='submit-btn'>SUBMIT ORDER</button>
+                            </div>
+                        </div>
+                        <div className='order'>
+                            <h3 className='order-title'>Order 1</h3>
+                            <div className='job-title-section sec'>
+                                <h4>JOB TITLE:</h4>
+                                <div className='j-title'>Lorem Ipsum</div>
+                            </div>
+                            <div className='copies-date-section sec'>
+                                <div className='page-section'>
+                                    <h4>PAGES:</h4>
+                                    <h6>20</h6>
+                                </div>
+                                <div className='copies-section'>
+                                    <h4>COPIES:</h4>
+                                    <h6>450</h6>
+                                </div>
+                                <div className='date-section'>
+                                    <h4>DATE DUE:</h4>
+                                    <h6>23/09/2022</h6>
+                                </div>
+                            </div>
+                            <div className='address-section sec'>
+                                <h4>ADDRESS:</h4>
+                                <div className='address'>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                        Ut tempor senectus arcu cursus tortor odio ornare </p>
+                                </div>
+                            </div>
+                            <div className='info-section sec'>
+                                <h4>ADDITIONAL INFORMATION:</h4>
+                                <div className='info'>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                    Ut tempor senectus arcu cursus tortor odio ornare velit 
+                                    blandit. Elit faucibus ante sed nascetur. Tincidunt </p>
+                                </div>
+                            </div>
+                            <div className='buttons-section sec'>
+                                <button className='edit-btn'>EDIT</button>
+                                <button className='submit-btn'>SUBMIT ORDER</button>
+                            </div>
+                        </div>
+                        <div className='order'>
+                            <h3 className='order-title'>Order 1</h3>
+                            <div className='job-title-section sec'>
+                                <h4>JOB TITLE:</h4>
+                                <div className='j-title'>Lorem Ipsum</div>
+                            </div>
+                            <div className='copies-date-section sec'>
+                                <div className='page-section'>
+                                    <h4>PAGES:</h4>
+                                    <h6>20</h6>
+                                </div>
+                                <div className='copies-section'>
+                                    <h4>COPIES:</h4>
+                                    <h6>450</h6>
+                                </div>
+                                <div className='date-section'>
+                                    <h4>DATE DUE:</h4>
+                                    <h6>23/09/2022</h6>
+                                </div>
+                            </div>
+                            <div className='address-section sec'>
+                                <h4>ADDRESS:</h4>
+                                <div className='address'>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                        Ut tempor senectus arcu cursus tortor odio ornare </p>
+                                </div>
+                            </div>
+                            <div className='info-section sec'>
+                                <h4>ADDITIONAL INFORMATION:</h4>
+                                <div className='info'>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                    Ut tempor senectus arcu cursus tortor odio ornare velit 
+                                    blandit. Elit faucibus ante sed nascetur. Tincidunt </p>
+                                </div>
+                            </div>
+                            <div className='buttons-section sec'>
                                 <button className='edit-btn'>EDIT</button>
                                 <button className='submit-btn'>SUBMIT ORDER</button>
                             </div>
